9a3d661b8d332866983b2951ddb5c41de1e20f81
brandonfrulla <bfrulla@mail.umw.edu>
2020-11-07 19:42:00 -0500

    modified:   assembly/switchStatement.s

--- assembly/switchStatement.s
+++ assembly/switchStatement.s
@@ -10,31 +10,50 @@ main:
     
     cmp r0, 0x1       // compare a to 1
     beq case1
-    cmp r0, 0x2
+    cmp r0, 0x2       // compare a to 2
     beq case2
-    cmp r0, 0x3
+    cmp r0, 0x3       // compare a to 3
     beq case3
-    cmp r0, 0x4
+    cmp r0, 0x4       // compare a to 4
     beq case4
-    cmp r0, 0x5
+    cmp r0, 0x5       // compare a to 5
     beq case5
-    cmp r0, 0x6
+    cmp r0, 0x6       // compare a to 6
     beq case6
-    cmp r0, 0x7
+    cmp r0, 0x7       // compare a to 7
     beq case7
     b caseDefault
+
 case1:
-    
+    add r0, r1, r2      // adding b & c, storing in a
+    str r0, [r13, #0]
+    endif    
 case2:
-
+    sub r0, r1, r2      // subtracting c from b, storing in a
+    str r0, [r13, #0]
+    endif
 case3:
-
+    mul r0, r1, r2      // multiplying b and c, storing in a
+    str r0, [r13, #0]
+    endif
 case4:
-
+    div r0, r1, r2      // dividing b and c, storing in a
+    str r0, [r13, #0]
+    endif
 case5:
-
+    and r0, r1, r2      // comparing b && c, storing in a
+    str r0, [r13, #0]  
+    endif
 case6:
-
+    orr r0, r1, r2      // comparing b || c, storing in a
+    str r0, [r13, #0]
+    endif
 case7:
-
+    eor r0, r1, r2      // comparing b ^ c, storing in a
+    str r0, [r13, #0]
+    endif
 caseDefault:
+    mov r0, #0          // make a = 0
+    str r0, [r13, #0]
+endif:
+    //blank af
