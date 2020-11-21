cddd691a168e83de2b0d809f9c37f6eddb8e3f22
brandonfrulla <bfrulla@mail.umw.edu>
2020-11-21 15:22:30 -0500

    modified:   cpu.c

--- cpu.c
+++ cpu.c
@@ -38,11 +38,72 @@ void show_regs() {
     printf("reg12: 0x%08x, reg13: 0x%08x, reg14: 0x%08x, reg15: 0x%08x\n", registers[12], registers[13], registers[14], registers[15]);
 }
 
+//fetch decode execute
 void step() {
+    int inst, reg1, reg2, reg3, reg0, reg, address, immediate, eq, lt, gt;
     int pc = registers[PC];
-    int inst; 
     system_bus(pc, &inst, READ);
-    printf("You need to implement step().\n");
+    int opcode = inst >> 24;
+    printf("PC: %x, Opcode: %x, Instruction: %x\n", pc, opcode, inst);
+    switch (opcode) { 
+    case LDR:
+        printf("LDR reached\n");
+        pc += 4;
+        break;
+    case STR:
+        printf("STR reached\n");
+        pc += 4;
+        break;
+    case LDX:
+        printf("LDX reached\n");
+        pc += 4;
+        break;
+    case STX:
+        printf("LDX reached\n");
+        pc += 4;
+        break;
+    case MOV:
+        printf("MOV reached\n");
+        pc += 4;
+        break;
+    case ADD:
+        printf("ADD reached\n");
+        pc += 4;
+        break;
+    case SUB:
+        printf("SUB reached\n");
+        pc += 4;
+        break;
+    case MUL:
+        printf("MUL reached\n");
+        pc += 4;
+        break;
+    case DIV:
+        printf("DIV reached\n");
+        pc += 4;
+        break;
+    case AND:
+        printf("AND reached\n");
+        pc += 4;
+        break;
+    case ORR:
+        printf("ORR reached\n");
+        pc += 4;
+        break;
+    case EOR:
+        printf("EOR reached\n");
+        pc += 4;
+        break;
+    case CMP:
+        printf("CMP reached\n");
+        pc += 4;
+        break;
+    case B:
+        printf("B reached\n");
+        //pc = address;
+        break;    
+    }
+    registers[15] = pc; 
 }
 
 void step_n(int n) {
@@ -57,6 +118,13 @@ void step_show_reg() {
 }
 
 void step_show_reg_mem() {
-    printf("You need to implement step_show_reg_mem().\n");
+    step_show_reg();
+    //some other method call to show memory
+
+    /*f. step_show_reg​ shall display ​step​’s display plus it shall display the values ofregisters changed by the instruction.
+    PC: 0x00000128, inst: 0x31000003, add r0, r0, r3CPSR: 0x40000000reg[0]: before: 0x00000064, after: 0x00000067reg[15]: before: 0x00000128, 
+    after: 0x0000012cg.step_show_reg_mem​ shall display ​step_show_reg​’s display plus it shall displaythe values of memory locations changed by 
+    the instruction.PC: 0x0000012c, inst: 0x12000104, str r0, 0x104CPSR: 0x40000000reg[15]: before: 0x0000012c, after: 0x00000130addr: 0x0104, 
+    before: 0x00000010, after: 0x00000067*/
 }
 
