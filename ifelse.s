30958c71c66c02201242ca0f567f8c1c84ff02e2
Luis Maldonado <lmaldona@mail.umw.edu>
2020-11-25 13:21:08 -0500

    ".s File Comments"

--- ifelse.s
+++ ifelse.s
@@ -6,16 +6,16 @@
         mov r0, #10      //
         mov r1, #20      //
         mov r2, #0       //
-        str r0, [r13,#0] //
-        str r1, [r13,#4] //
-        str r2, [r13,#8] //
+        str r0, [r13,#0] // a = 10
+        str r1, [r13,#4] // b = 20
+        str r2, [r13,#8] // c = 0
         ldr r0, [r13,#0] //
-        ldr r1, [r13,#4] // 
+        ldr r1, [r13,#4] //
         ldr r2, [r13,#8] //
-        cmp r0, r1       //
+        cmp r0, r1       // a < b
         bge else         //
-        str r0, [r13,#8] //
+        str r0, [r13,#8] // c = a
         b endif          //
 else:                    //
-        str r1, [r13,#8] //
-endif:                   //
+        str r1, [r13,#8] // c = b
+endif:                   // END of program
